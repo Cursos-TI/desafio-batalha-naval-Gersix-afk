6ae9d7c09e7eac7a10092ea0d9d8ec8089eb2f9d
Gersix-afk <gersonribeiro172@gmail.com>
2025-04-03 19:23:03 +0000

    NOVA LINHA

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,20 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#Define SIZE 10 // Tamanho do tabuleiro
+#Define CONE_SIZE 5 //  Define Tamanho da matriz de habilidade Cone
+#Define CROSS_SIZE 5 // Define Tamanho da habilidade Cruz
+#Define OCTAHEDRON_SIZE 5 // Define Tamanho da habilidade Octaedro
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+// Função para innicializar o tabuleiro com água (0)
+void initializeBoard (int board[SIZE] [SIZE]){
+    for (int i = 0; i< SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            board [i][i = 0; // 0 representa água]
+        }
+    }
+} 
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+// Função para exibir Tabuleiro
+void displayBoard ((int board [SIZE] [SIZE]){
+    for (int i =  0; )
 }
